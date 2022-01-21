41364168dcb8cb7f5bd1936a3451426a68706de2
plagymus <etiennecombes@gmail.com>
2022-01-21 11:01:02 +0100

    Petites modifications 3

--- plotquick.py
+++ plotquick.py
@@ -13,32 +13,25 @@ from math import sqrt
 
 
 
-p=256
+p=128
 
-c1=1E-5
-c2=1E-5
-k1=c1
-k2=c2
+k1=1e5
+k2=1e5
 ka=(1/6)*(sqrt(25*k1**2-14*k1+25)-5*k1+5)
 kb=(1/6)*(sqrt(25*k2**2-14*k2+25)-5*k2+5)
 
     
-it=400
+it=100
 micro=1
 
-itplot1=0
-itplot2=100
-
-KA=max(c1,c2,ka,kb,1)
-KB=min(c1,c2,ka,kb,1)
+KA=max(k1,k2,ka,kb,1)
+KB=min(k1,k2,ka,kb,1)
 
 
 kms=0.5*(KA+KB)
 kem=sqrt(KA*KB)
 
 K=["MS kms","EM kem","EM khom","MSada1 khom","EMada1 khom"]
-
-
 
 U0=[]
 U1=[]
@@ -48,15 +41,15 @@ ER=[]
 for i in [0,1,2,3,4]:
     
     if i==0:
-        U=ada1(p,micro,c1,c2,kms,0,0,it)  #MS simple
+        U=ada1(p,micro,k1,k2,kms,0,0,it)  #MS simple
     if i==1:
-        U=ada1(p,micro,c1,c2,kem,1,0,it)  #EM simple
+        U=ada1(p,micro,k1,k2,kem,1,0,it)  #EM simple kem
     if i==2:
-        U=ada1(p,micro,c1,c2,1,1,0,it)  #EM simple
+        U=ada1(p,micro,k1,k2,1,1,0,it)  #EM simple khom
     if i==3:
-        U=ada1(p,micro,c1,c2,1,0,1,it)    #MS ame 1 par
+        U=ada1(p,micro,k1,k2,1,0,1,it)    #MS ame 1 par
     if i==4:
-        U=ada1(p,micro,c1,c2,1,1,1,it)    #EM ame 1 par
+        U=ada1(p,micro,k1,k2,1,1,1,it)    #EM ame 1 par
 
     U0.append(U[0])
     U1.append(U[1])
@@ -71,7 +64,7 @@ itplot2=5000
     
 for i in range(len(K)):
     plt.plot([t for t in range(len(U0[i]))[itplot1:itplot2]],[abs(x-1) for x in U0[i][itplot1:itplot2]],label=K[i])
-plt.title("Evolutions de log(Cth-Chom/Cth) <CA> pour les différents algorithmes  Discrétisation:"+str(p)+"  C1: "+str(c1)+"  C2: "+str(c2)+"  k3: ")
+plt.title("(Kth-Kth/Kth) <CA>  Discrétisation:"+str(p)+"  k1: "+str(k1)+"  k2: "+str(k2))
 ax = plt.subplot(111)
 ax.set_yscale('log')
 box = ax.get_position()
@@ -81,7 +74,7 @@ plt.show()
 
 for i in range(len(K)):
     plt.plot([t for t in range(len(U1[i]))[itplot1:itplot2]],[abs(x-1) for x in U1[i][itplot1:itplot2]],label=K[i])
-plt.title("Evolutions de log(Cth-Chom/Cth) <tACA>   Discrétisation:"+str(p)+"  C1: "+str(c1)+"  C2: "+str(c2)+"  k3: ")
+plt.title("(Kth-Kth/Kth) <tACA>   Discrétisation:"+str(p)+"  k1: "+str(k1)+"  k2: "+str(k2))
 ax = plt.subplot(111)
 ax.set_yscale('log')
 box = ax.get_position()
@@ -91,7 +84,7 @@ plt.show()
 
 for i in range(len(K)):
     plt.plot([t for t in range(len(U2[i]))[itplot1:itplot2]],U2[i][itplot1:itplot2],label=K[i])   
-plt.title("Evolutions de l'ecart    Discrétisation:"+str(p)+"  C1: "+str(c1)+"  C2: "+str(c2)+"  k3: ")
+plt.title("Ecart   Discrétisation:"+str(p)+"  k1: "+str(k1)+"  k2: "+str(k2))
 ax = plt.subplot(111)
 ax.set_yscale('log')
 box = ax.get_position()
@@ -102,7 +95,7 @@ plt.show()
 
 for i in range(len(K)):
     plt.plot([t for t in range(len(ER[i]))[itplot1:itplot2]],ER[i][itplot1:itplot2],label=K[i])   
-plt.title("Evolutions de l'erreur    Discrétisation:"+str(p)+"  C1: "+str(c1)+"  C2: "+str(c2)+"  k3: ")
+plt.title("Erreur    Discrétisation:"+str(p)+"  k1: "+str(k1)+"  k2: "+str(k2))
 ax = plt.subplot(111)
 ax.set_yscale('log')
 box = ax.get_position()
