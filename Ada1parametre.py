178c6ab084cd9396fc7e793a9d7c0e8267a484ee
plagymus <etiennecombes@gmail.com>
2022-03-08 14:09:13 +0100

    Ajout des initialisations différentes

--- Ada1parametre.py
+++ Ada1parametre.py
@@ -14,9 +14,9 @@ from math import sqrt
 #Algo le choix de l'algo: 0 pour conditionnement MS  ou 1 pour EM
 # AME=0 pour les algoritmes basiques,AME=1 pour les algoritmes améliorés, AME=2 ppur les algoritmes améliorés bis, 3 pour MS ada ter, AME=4 pour le GC (marche avec MS)
 #Prec est le nombre d'itérations
+#Ini=0 pour Voigt et 1 pour initialisation de Reuss
 
-
-def ada1(N0,Micro,k1,k2,K0,Algo,AME,Prec):
+def ada1(N0,Micro,k1,k2,K0,Algo,AME,Prec,Ini):
     
     CA,ACA,ER,ALPHA=[],[],[],[]
 
@@ -33,6 +33,12 @@ def ada1(N0,Micro,k1,k2,K0,Algo,AME,Prec):
     filter_level=2
     k0=K0*np.eye(d)
     
+    kmoyinv=0*np.eye(d)
+    for i in np.ndindex(tuple(N)):
+        kmoyinv+=np.linalg.inv(ki[i])/(N[0]**d)     #<K^-1>, utile pour initialisation de Reuss
+    print("kmoyinv= ",kmoyinv)
+    
+    
     alpha=np.zeros(tuple(N)+(d,d))  #variable stockant le champ identité (conditionnement MS) ou 2*(C+C0)^(-1).C0 (conditionnement EM)
     for i in np.ndindex(tuple(N)):
         if Algo==0:
@@ -40,27 +46,59 @@ def ada1(N0,Micro,k1,k2,K0,Algo,AME,Prec):
         if Algo==1:
             alpha[i]=2*np.dot(np.linalg.inv(ki[i]+k0), k0)
 
-    
     A=np.zeros(tuple(N)+(d,d))   #matrice de localisation, tableau de dimension N[0]*2 de stockage des gradients de température (vecteurs), pour les deux cas de chargements [1,0] et [0,1]
             
     for j in range(d):                             #on itère sur les 2 directions
         
         it=0
         
-        Eps_field = np.zeros(tuple(N)+(d,))        #initialisation du champ de déformation
-        E_field = np.zeros(tuple(N)+(d,))          #initialisation du champ qui contient le chargement
+        Eps_field = np.zeros(tuple(N)+(d,))          #initialisation du champ de déformation
+        Eps_field_V = np.zeros(tuple(N)+(d,))        #initialisation du champ de déformation Voigt
+        Eps_field_R = np.zeros(tuple(N)+(d,))        #initialisation du champ de déformation Reuss
+        E_field = np.zeros(tuple(N)+(d,))            #initialisation du champ qui contient le chargement
+        X_V= np.zeros(tuple(N)+(d,))
+        X_R= np.zeros(tuple(N)+(d,))
         X= np.zeros(tuple(N)+(d,))
         Y= np.zeros(tuple(N)+(d,))
         
         for i in np.ndindex(tuple(N)):                     #Champ E (chargement)
                 E_field[i+(j,)]=1.
-        Eps_field=np.copy(E_field)                         #initialisation: Eps=chargement
-        
+                
+        Eps_field_V=np.copy(E_field)                         #initialisation Voigt: Eps=chargement
         for i in np.ndindex(tuple(N)):
-                X[i]=np.dot(ki[i]-k0, Eps_field[i])             #calcul de tau (début de la récursion)
+            X_V[i]=np.dot(ki[i]-k0, E_field[i])             #calcul de tau (début de la récursion)
+        for i in np.ndindex(tuple(N)):                      #initialisation Reuss
+            Eps_field_R[i]=np.dot(np.dot(np.linalg.inv(ki[i]),np.linalg.inv(kmoyinv)),E_field[i])
+        for i in np.ndindex(tuple(N)):
+            X_R[i]=np.dot(ki[i]-k0, Eps_field_R[i]) 
             
         field,field_fourier,fft,ifft,tupleK,frequencies,filters,tupleFilters = initializeGreen(N,filter_level=filter_level)
-        X=operate_field(X,field_fourier,fft,ifft,tupleK,N,frequencies,filter_level,filters,tupleFilters,k0)   #calcul de - la convolée de tau par gamma0
+        X_V=operate_field(X_V,field_fourier,fft,ifft,tupleK,N,frequencies,filter_level,filters,tupleFilters,k0)   #calcul de - la convolée de tau par gamma0
+        field,field_fourier,fft,ifft,tupleK,frequencies,filters,tupleFilters = initializeGreen(N,filter_level=filter_level)
+        X_R=operate_field(X_R,field_fourier,fft,ifft,tupleK,N,frequencies,filter_level,filters,tupleFilters,k0)   #calcul de - la convolée de tau par gamma0
+        X_R=E_field-Eps_field_R+X_R
+        
+        if Ini==0:
+            X=np.copy(X_V)
+            Eps_field=np.copy(Eps_field_V)
+        if Ini==1:
+            X=np.copy(X_R)
+            Eps_field=np.copy(Eps_field_R)
+        if Ini==2:
+            l_V=0
+            l_R=0
+            X_RR,X_RV,X_X=0,0,0
+            for i in np.ndindex(tuple(N)):
+                X_RR+=np.dot(X_R[i],X_R[i])
+                X_RV+=np.dot(X_R[i],X_V[i])
+                X_X+=np.dot(X_V[i]-X_R[i],X_V[i]-X_R[i])
+            l_V=(X_RR-X_RV)/X_X
+            l_R=1-l_V
+            print("l_V, l_R :", l_V,l_R)
+            Eps_field=l_V*Eps_field_V+l_R*Eps_field_R
+            X=l_V*X_V+l_R*X_R
+            
+        
         if AME==4:
             P=np.copy(X)
             
@@ -190,7 +228,7 @@ def ada1(N0,Micro,k1,k2,K0,Algo,AME,Prec):
     print("deltaCA ",deltaCA)
     deltaACA=(ACA[len(ACA)-1]-ACA[len(ACA)-2])/ACA[len(ACA)-2]
     print("deltaACA ",deltaACA)
-    legend="??"
+    legend="Quel algo?"
     if Algo==0 and AME==0:
         legend="MS"
     if Algo==1 and AME==0:
@@ -205,7 +243,14 @@ def ada1(N0,Micro,k1,k2,K0,Algo,AME,Prec):
         legend="MSada1ter"
     if Algo==0 and AME==4:
         legend="MS GC"
+    
+    if Ini==0:
+        initial="Ini Voigt"
+    if Ini==1:
+        initial="Ini Reuss"
+    if Ini==2:
+        initial="Ini Moyenne"
         
-    figures(Eps_field[:,:,1], "e22 "+legend+"  k0="+str(K0)) 
+    figures(Eps_field[:,:,1], "e22 "+legend+"  k0="+str(K0)+" "+initial) 
     
     return CA,ACA,ER,ALPHA,krefCA[0][0],krefACA[0][0]
